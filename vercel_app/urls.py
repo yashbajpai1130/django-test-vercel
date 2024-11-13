54f7684f7a8c38cac7d89cebe6701e595e68378e
Yash <yash.bajpai@antiersolutions.com>
2024-11-13 16:17:09 +0530

    endpoints added

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('write/', views.write, name = 'write'),
+    path('get_last_name/', views.get_last_name, name = 'get_last_name')
 ]
--- vercel_app/views.py
+++ vercel_app/views.py
@@ -1,4 +1,21 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
 def index(request):
     return HttpResponse("Hello, world! This is my Django app.")
+@csrf_exempt
+def write(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        name = data['name']
+        with open("names.txt", "w+") as f:
+            f.write(name)
+
+        return JsonResponse({"message" : "name written"})
+@csrf_exempt
+def get_last_name(request):
+    if request.method == "GET":
+        with open("names.txt", "r") as f:
+            name = f.read()
+        return JsonResponse({"Last entered name" : name})
